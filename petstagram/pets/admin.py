5cc84a14db8ec8de4829b2e513afccec5306ef9f
Elena Borisova <elenaborrisova@gmail.com>
2021-03-16 01:15:34 +0200

    add models and views to Petstagram

---
+++ petstagram/pets/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+
+from pets.models import Pet, Like
+
+
+class LikeInlineAdmin(admin.TabularInline):
+    model = Like
+
+
+class PetAdmin(admin.ModelAdmin):
+    list_display = ('id', 'type', 'name', 'age')
+    list_filter = ('type', 'age')
+    inlines = [
+        LikeInlineAdmin,
+    ]
+
+
+admin.site.register(Pet, PetAdmin)
+admin.site.register(Like)
--- petstagram/petstagram/settings.py
+++ petstagram/petstagram/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'common',
+    'pets',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +75,12 @@ WSGI_APPLICATION = 'petstagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'petstagram',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
