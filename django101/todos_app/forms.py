3beef3c668b56c065be47ece26408cb320772eb3
Elena Borisova <elenaborrisova@gmail.com>
2021-03-20 01:58:49 +0200

    modify forms for Todo List

--- django101/todos_app/forms.py
+++ django101/todos_app/forms.py
@@ -2,19 +2,14 @@ from django import forms
 
 
 class TodoForm(forms.Form):
-    title = forms.CharField(
-        max_length=30,
-        widget=forms.TextInput(
-            attrs={
-                'class': 'form-control',
-            }
-        )
-    )
-    description = forms.CharField(
-        widget=forms.Textarea(
-            attrs={
-                'class': 'form-control',
-            }
-        ),
-        required=False,
-    )
+    title = forms.CharField(max_length=30)
+    description = forms.CharField()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for _, field in self.fields.items():
+            if 'class' in field.widget.attrs:
+                value = field.widget.attrs['class'] + ' form-control'
+            else:
+                value = 'form-control'
+            field.widget.attrs['class'] = value
